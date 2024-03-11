705116ea8858615d85392e7da5fc22bd3f23d63d
TODO <TODO>
2024-03-11 14:29:35 +0100

    question 3

--- tp-refactoring/log-hs/app/Main.hs
+++ tp-refactoring/log-hs/app/Main.hs
@@ -1,3 +1,5 @@
+import System.IO
+
 add3 :: Int -> Int 
 add3 = (+3)
 
@@ -12,9 +14,22 @@ mycompute v0 = do
     let v2 = mul2 v1
     return v2
 
+mycomputefile :: Int -> IO Int
+mycomputefile v0 =do
+    h <- openFile "log.txt" WriteMode
+    hPutStrLn h("add3" ++ show v0)
+    let v1 = add3 v0
+    hPutStrLn h("mul2" ++ show v1)
+    let v2 =mul2 v1
+    hClose h
+    return v2
+
 main :: IO ()
 main = do
     putStrLn "this is log-hs"
     
     res <- mycompute 18
     print res
+
+    resFile <- mycomputefile 18
+    print resFile
