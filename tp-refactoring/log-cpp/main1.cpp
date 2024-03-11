944ef4759236968da998bf51c1cb35482a45ed5b
TODO <TODO>
2024-03-11 15:41:06 +0100

    question 4

--- tp-refactoring/log-cpp/main1.cpp
+++ tp-refactoring/log-cpp/main1.cpp
@@ -18,11 +18,25 @@ int mycompute(int v0) {
     return v2;
 }
 
+int mycomputeFile(int v0){
+    std::ofstream ofs("test.txt");
+    ofs<< "add3 " + std::to_string(v0) << std::endl;
+    const int v1 = add3(v0);
+    std::cout << "mul2 " + std::to_string(v1) << std::endl;
+    const int v2 = add3(v1);
+    return v2;
+    
+
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
 
     const int res = mycompute(18);
     std::cout << res << std::endl;
+
+    const int resF = mycomputeFile(18);
+    std::cout <<resF <<std::endl;
 
     return 0;
 }
--- tp-refactoring/log-cpp/main2.cpp
+++ tp-refactoring/log-cpp/main2.cpp
@@ -12,8 +12,23 @@ int mul2(int n) {
     return n*2;
 }
 
+int mycompute(logFunc_t logF, int v0){
+    logF ("add3 " + std::to_string(v0) +"\n");
+    const int v1 = add3(v0);
+    logF("mul2 " + std::to_string(v1) +"\n");
+    const int v2 = add3(v1);
+    return v2;
+}
+
+void f1(const std::string & str){
+    std::cout <<str;
+}
+
 int main() {
     std::cout << "this is log-cpp" << std::endl;
+
+    const int res = mycompute(f1,18);
+    std::cout << res << std::endl;
 
     return 0;
 }
